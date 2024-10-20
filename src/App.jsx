f29f318647ad5ed856f07ae2792d87dd0289c006
TheSilentOwl <115552526+TheSilentOwl@users.noreply.github.com>
2024-10-20 21:27:40 +0300

    initialize my portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="bg-slate-200 flex justify-center items-center h-screen">
+      <p className="text-red-600 font-bold text-5xl">Hello World</p>
+    </div>
+  );
+}
